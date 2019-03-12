6dc8c18abf152e65e84c68d4f67233b3c6a8c869
audrey chen <audreychen97@gmail.com>
2019-03-12 13:22:47 +0100

    updated controller

--- app/models/scooter.rb
+++ app/models/scooter.rb
@@ -3,4 +3,5 @@ class Scooter < ApplicationRecord
   has_many :reservations
   has_many :users, through: :reservations
   has_many :reviews, through: :reservations
+  mount_uploader :photo, PhotoUploader
 end
